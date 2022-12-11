74cebd484967c78567cc87d1acfaf43f350a4a67
r0man <roman@burningswell.com>
2022-12-11 13:25:02 +0100

    Build asahi-m1n1

---
+++ asahi/packages.scm
@@ -0,0 +1,59 @@
+(define-module (asahi packages)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages imagemagick)
+  #:use-module (gnu packages rust)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix git-download)
+  #:use-module (guix packages)
+  #:use-module (nongnu packages linux))
+
+(define-public asahi-linux
+  (package
+    (inherit linux)
+    (name "asahi-linux")
+    (version "0.5.2")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/AsahiLinux/linux.git")
+             (commit "6ecde4985ffc8490eb3dbc9303d338b8872e3991")))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "05r2i3dnwa9v35x93p6r6ixnf456annfx498jgmviwl53jkxi1qc"))))
+    (home-page "https://asahilinux.org")
+    (synopsis "Linux on Apple Silicon")
+    (description "Asahi Linux is a project and community with the goal of porting Linux
+to Apple Silicon Macs, starting with the 2020 M1 Mac Mini, MacBook
+Air, and MacBook Pro.")))
+
+(define-public asahi-m1n1
+  (package
+    (name "asahi-m1n1")
+    (version "1.2.3")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/AsahiLinux/m1n1.git")
+             (commit "fb36e302363cbe505da856f127bc0ae5e4a11257")))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0fm8vhdi75nfwm3n9wy5hrg26wa10h2bgba3250jfv2lg62rsd50"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:phases
+       (modify-phases %standard-phases
+         (delete 'configure)
+         (replace 'install
+           (lambda* (#:key outputs #:allow-other-keys)
+             (let ((dir (string-append (assoc-ref outputs "out") "/usr/lib/")))
+               (mkdir-p dir)
+               (invoke "echo" (string-append dir "m1n1.bin"))
+               (copy-file "build/m1n1.bin" (string-append dir "m1n1.bin"))
+               (copy-file "build/m1n1.macho" (string-append dir "m1n1.macho"))))))))
+    (native-inputs (list rust))
+    (home-page "https://github.com/AsahiLinux/m1n1")
+    (synopsis "Experimentation playground for Apple Silicon")
+    (description "A bootloader and experimentation playground for Apple Silicon")
+    (license license:expat)))

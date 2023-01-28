3bb8944184340be1217464f63fc69adbd8dd64a0
r0man <roman@burningswell.com>
2023-01-28 22:33:59 +0000

    Split installer into base and edge (#26)

--- asahi/guix/install/base.scm
+++ asahi/guix/install/base.scm
@@ -1,22 +1,12 @@
-(define-module (asahi guix installer)
+(define-module (asahi guix install base)
   #:use-module (asahi guix initrd)
   #:use-module (asahi guix packages)
   #:use-module (asahi guix services)
   #:use-module (gnu bootloader grub)
   #:use-module (gnu bootloader)
   #:use-module (gnu packages bootloaders)
-  #:use-module (gnu packages curl)
-  #:use-module (gnu packages emacs)
-  #:use-module (gnu packages linux)
-  #:use-module (gnu packages mtools)
-  #:use-module (gnu packages package-management)
-  #:use-module (gnu packages version-control)
-  #:use-module (gnu services)
   #:use-module (gnu system install)
-  #:use-module (gnu system linux-initrd)
   #:use-module (gnu system)
-  #:use-module (guix git-download)
-  #:use-module (guix)
   #:export (asahi-installation-os))
 
 (define asahi-installation-os
@@ -37,10 +27,4 @@
        "quiet"))
     (services (cons* %channels-service (operating-system-user-services installation-os)))))
 
-(define asahi-installation-os-edge
-  (operating-system
-    (inherit asahi-installation-os)
-    (kernel asahi-linux-edge)
-    (initrd-modules asahi-initrd-modules-edge)))
-
-asahi-installation-os-edge
+asahi-installation-os
---
+++ asahi/guix/install/edge.scm
@@ -0,0 +1,14 @@
+(define-module (asahi guix install edge)
+  #:use-module (asahi guix initrd)
+  #:use-module (asahi guix install base)
+  #:use-module (asahi guix packages)
+  #:use-module (gnu system)
+  #:export (asahi-installation-os-edge))
+
+(define asahi-installation-os-edge
+  (operating-system
+    (inherit asahi-installation-os)
+    (kernel asahi-linux-edge)
+    (initrd-modules asahi-initrd-modules-edge)))
+
+asahi-installation-os-edge

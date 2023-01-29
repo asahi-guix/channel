ed604b70aaffb3cf06e485b4634cd03519ffce59
r0man <roman@burningswell.com>
2023-01-29 14:49:29 +0000

    Add asahi-audio package (#28)

--- asahi/guix/packages.scm
+++ asahi/guix/packages.scm
@@ -26,6 +26,39 @@
   #:use-module (guix utils)
   #:use-module (srfi srfi-1))
 
+(define-public asahi-audio
+  (package
+    (name "asahi-audio")
+    (version "5f9067d0fba89acb6c6d68819edad30fe28b1dfe")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/chadmed/asahi-audio.git")
+                    (commit version)))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32 "0048qx4afvm1qfayzzfia7iqbj17pkz5xspya74xlc5ji3k3vfij"))))
+    (build-system copy-build-system)
+    (arguments
+     `(#:install-plan
+       '(("conf" "etc/pipewire/pipewire.conf.d")
+         ("firs" "usr/share/pipewire/devices/apple"))
+       #:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack 'patch-source
+           (lambda* (#:key outputs #:allow-other-keys)
+             (let ((out (assoc-ref outputs "out")))
+               (substitute* "conf/j314.conf"
+                 (("/usr/share/pipewire/devices/apple")
+                  (string-append out "/usr/share/pipewire/devices/apple")))
+               (substitute* "conf/j316.conf"
+                 (("/usr/share/pipewire/devices/apple")
+                  (string-append out "/usr/share/pipewire/devices/apple")))))))))
+    (home-page "https://github.com/chadmed/asahi-audio")
+    (synopsis "Linux audio configuration for Apple Silicon Macs.")
+    (description "Linux userspace audio configuration for Apple Silicon Macs.")
+    (license license:expat)))
+
 (define-public alsa-ucm-conf-asahi
   (package
     (name "alsa-ucm-conf-asahi")

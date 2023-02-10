e2ae0c67ebdeb182fb3d67a012e0c1f58ea887d3
r0man <roman@burningswell.com>
2023-02-10 15:53:09 +0100

    Add replace-mesa

--- asahi/guix/transformations.scm
+++ asahi/guix/transformations.scm
@@ -1,8 +1,13 @@
 (define-module (asahi guix transformations)
+  #:use-module ((asahi guix packages))
   #:use-module ((asahi guix packages jemalloc) #:prefix next:)
+  #:use-module (gnu packages gl)
   #:use-module (gnu packages jemalloc)
   #:use-module (guix packages)
   #:export (replace-jemalloc))
 
 (define replace-jemalloc
   (package-input-rewriting `((,jemalloc . ,next:jemalloc))))
+
+(define replace-mesa
+  (package-input-rewriting `((,mesa . ,mesa-asahi-edge))))

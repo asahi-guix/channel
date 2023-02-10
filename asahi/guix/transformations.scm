813422433ccd77431bcb0acff8ea865776aa110f
r0man <roman@burningswell.com>
2023-02-10 20:32:09 +0100

    Add replace-libdrm

--- asahi/guix/transformations.scm
+++ asahi/guix/transformations.scm
@@ -3,11 +3,17 @@
   #:use-module ((asahi guix packages jemalloc) #:prefix next:)
   #:use-module (gnu packages gl)
   #:use-module (gnu packages jemalloc)
+  #:use-module (gnu packages xdisorg)
   #:use-module (guix packages)
-  #:export (replace-jemalloc replace-mesa))
+  #:export (replace-jemalloc
+            replace-libdrm
+            replace-mesa))
 
 (define replace-jemalloc
   (package-input-rewriting `((,jemalloc . ,next:jemalloc))))
+
+(define replace-libdrm
+  (package-input-rewriting `((,libdrm . ,libdrm-2-4-114))))
 
 (define replace-mesa
   (package-input-rewriting `((,mesa . ,mesa-asahi-edge))))

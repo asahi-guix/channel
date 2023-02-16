7785d17f5927dbb026c217928509dc5a2dd15c99
r0man <roman@burningswell.com>
2023-02-16 19:11:55 +0100

    Add udev rules

---
+++ asahi/guix/udev.scm
@@ -0,0 +1,24 @@
+(define-module (asahi guix udev)
+  #:use-module (gnu services base)
+  #:export (%udev-backlight-brightness-rule
+            %udev-kbd-backlight-brightness-rule))
+
+(define %udev-backlight-brightness-rule
+  (udev-rule
+   "90-backlight-brightness.rules"
+   (string-append
+    "ACTION==\"add\", SUBSYSTEM==\"backlight\", "
+    "RUN+=\"/run/current-system/profile/bin/chgrp video /sys/class/backlight/%k/brightness\""
+    "\n"
+    "ACTION==\"add\", SUBSYSTEM==\"backlight\", "
+    "RUN+=\"/run/current-system/profile/bin/chmod g+w /sys/class/backlight/%k/brightness\"")))
+
+(define %udev-kbd-backlight-brightness-rule
+  (udev-rule
+   "90-kbd-backlight-brightness.rules"
+   (string-append
+    "ACTION==\"add\", SUBSYSTEM==\"leds\", "
+    "RUN+=\"/run/current-system/profile/bin/chgrp video /sys/class/leds/kbd_backlight/brightness\""
+    "\n"
+    "ACTION==\"add\", SUBSYSTEM==\"leds\", "
+    "RUN+=\"/run/current-system/profile/bin/chmod g+w /sys/class/leds/kbd_backlight/brightness\"")))

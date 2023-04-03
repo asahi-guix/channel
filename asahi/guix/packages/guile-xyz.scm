38f8b5c73fd75be0edf2cc02e5c63865c77a23e5
Roman Scherer <roman@burningswell.com>
2023-04-03 20:33:20 +0200

    Add guile-asahi-guix

---
+++ asahi/guix/packages/guile-xyz.scm
@@ -0,0 +1,38 @@
+(define-module (asahi guix packages guile-xyz)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages guile)
+  #:use-module (gnu packages guile-xyz)
+  #:use-module (gnu packages package-management)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages texinfo)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix build-system guile)
+  #:use-module (guix gexp)
+  #:use-module (guix git-download)
+  #:use-module (guix packages))
+
+(define-public guile-asahi-guix
+  (let ((commit "f3446ac235ec537861cbfb9d2508207e9a2e2d3c")
+        (revision "1"))
+    (package
+      (name "guile-asahi-guix")
+      (version (git-version "0.0.1" revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/r0man/asahi-guix.git")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "16r64f7nj72x1zry0xrvpdsqcr0yb3pjyrfkh8ix9k481mabzrcj"))))
+      (build-system guile-build-system)
+      (native-inputs
+       (list autoconf automake guile-hall pkg-config texinfo))
+      (inputs (list guile-3.0-latest))
+      (propagated-inputs (list guix))
+      (synopsis "Asahi Guix")
+      (description "Asahi Linux on GNU Guix")
+      (home-page "https://github.com/r0man/asahi-guix")
+      (license license:gpl3+))))

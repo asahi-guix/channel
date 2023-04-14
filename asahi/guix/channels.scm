c410f6289e0fde9a8bd8a571d261a0a31c5088ea
Roman Scherer <roman@burningswell.com>
2023-04-14 15:32:37 +0200

    Use main branch in channels.scm

--- asahi/guix/channels.scm
+++ asahi/guix/channels.scm
@@ -6,7 +6,7 @@
   (list (channel
          (name 'asahi)
          (url "https://github.com/r0man/asahi-guix.git")
-         (branch "firmware-service")
+         (branch "main")
          (introduction
           (make-channel-introduction
            "d8f93a8a57be3e38f13916b4e5f3e1ad5ccee9de"

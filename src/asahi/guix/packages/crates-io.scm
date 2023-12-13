5ed5f457355e01fa64e2b50919d5850413e7bf5d
Roman Scherer <roman@burningswell.com>
2023-12-13 18:53:16 +0100

    Fix crates

--- src/asahi/guix/packages/crates-io.scm
+++ src/asahi/guix/packages/crates-io.scm
@@ -10,6 +10,71 @@
   #:use-module (guix git-download)
   #:use-module (guix packages))
 
+(define-public rust-prettyplease-0.2
+  (package
+    (name "rust-prettyplease")
+    (version "0.2.15")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "prettyplease" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "17az47j29q76gnyqvd5giryjz2fp7zw7vzcka1rb8ndbfgbmn05f"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-proc-macro2" ,rust-proc-macro2-1)
+                       ("rust-syn" ,rust-syn-2))))
+    (home-page "https://github.com/dtolnay/prettyplease")
+    (synopsis "A minimal `syn` syntax tree pretty-printer")
+    (description
+     "This package provides a minimal `syn` syntax tree pretty-printer")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-yansi-term-0.1
+  (package
+    (name "rust-yansi-term")
+    (version "0.1.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "yansi-term" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1w8vjlvxba6yvidqdvxddx3crl6z66h39qxj8xi6aqayw2nk0p7y"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-serde" ,rust-serde-1)
+                       ("rust-winapi" ,rust-winapi-0.3))))
+    (home-page "https://github.com/botika/yansi-term")
+    (synopsis "Library for ANSI terminal colours and styles (bold, underline)")
+    (description
+     "Library for ANSI terminal colours and styles (bold, underline)")
+    (license license:expat)))
+
+(define-public rust-annotate-snippets-0.9
+  (package
+    (name "rust-annotate-snippets")
+    (version "0.9.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "annotate-snippets" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "07p8r6jzb7nqydq0kr5pllckqcdxlyld2g275v425axnzffpxbyc"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-unicode-width" ,rust-unicode-width-0.1)
+                       ("rust-yansi-term" ,rust-yansi-term-0.1))))
+    (home-page "https://github.com/rust-lang/annotate-snippets-rs")
+    (synopsis "Library for building code annotations")
+    (description "Library for building code annotations")
+    (license (list license:asl2.0 license:expat))))
+
 (define-public rust-bindgen-0.69
   (package
     (name "rust-bindgen")
@@ -72,6 +137,337 @@
     (description
      "Automatically generates Rust FFI bindings to C and C++ libraries.")
     (license license:bsd-3)))
+
+(define-public rust-biquad-0.4
+  (package
+    (name "rust-biquad")
+    (version "0.4.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "biquad" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0gpc13lag439nmq077wfwz055qbjaxbpk7znvnbddbg3wgsj81c2"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-libm" ,rust-libm-0.1))))
+    (home-page "https://github.com/korken89/biquad-rs")
+    (synopsis
+     "A library for digital second order IIR filtrers, also known as biquads.")
+    (description
+     "This package provides a library for digital second order IIR filtrers, also
+known as biquads.")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-lv2-worker-0.1
+  (package
+    (name "rust-lv2-worker")
+    (version "0.1.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "lv2-worker" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "14crsrnjyarra9ipma6lhaj4gpfadvippzr134nkn0z3y30ip4fj"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-lv2-core" ,rust-lv2-core-3)
+                       ("rust-lv2-sys" ,rust-lv2-sys-2)
+                       ("rust-urid" ,rust-urid-0.1))))
+    (home-page "https://github.com/RustAudio/rust-lv2")
+    (synopsis "rust-lv2's work offloading library")
+    (description "rust-lv2's work offloading library")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-lv2-urid-2
+  (package
+    (name "rust-lv2-urid")
+    (version "2.1.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "lv2-urid" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0s2fcb0nyn54ml6azkbhnnxghy898x1q5vs5qgdznrhy9m20624c"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-lv2-core" ,rust-lv2-core-3)
+                       ("rust-lv2-sys" ,rust-lv2-sys-2)
+                       ("rust-urid" ,rust-urid-0.1))))
+    (home-page "https://github.com/RustAudio/rust-lv2")
+    (synopsis "rust-lv2's URID handling library")
+    (description "rust-lv2's URID handling library")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-lv2-time-0.1
+  (package
+    (name "rust-lv2-time")
+    (version "0.1.3")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "lv2-time" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0wznk17vvn5dph6r47vjwmf7g98pb6ij2fdhizdk95sf2qvkf82c"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-lv2-sys" ,rust-lv2-sys-2)
+                       ("rust-urid" ,rust-urid-0.1))))
+    (home-page "https://github.com/RustAudio/rust-lv2")
+    (synopsis "rust-lv2's wrapper of LV2's time types")
+    (description "rust-lv2's wrapper of LV2's time types")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-lv2-state-2
+  (package
+    (name "rust-lv2-state")
+    (version "2.0.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "lv2-state" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0nm0fc7cb4rkmfsvvr4xbac4qf0j7wl2gws3qrcflx057i2lpsb5"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-lv2-atom" ,rust-lv2-atom-2)
+                       ("rust-lv2-core" ,rust-lv2-core-3)
+                       ("rust-lv2-sys" ,rust-lv2-sys-2)
+                       ("rust-urid" ,rust-urid-0.1))))
+    (home-page "https://github.com/RustAudio/rust-lv2")
+    (synopsis "rust-lv2's state handling library")
+    (description "rust-lv2's state handling library")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-wmidi-3
+  (package
+    (name "rust-wmidi")
+    (version "3.1.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "wmidi" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1kxnbs18nmpzm2hfwaaa5h2s77cmk5w53srzxqmrqlkdpdcrjafa"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t))
+    (home-page "https://github.com/RustAudio/wmidi")
+    (synopsis "Midi parsing library.")
+    (description "Midi parsing library.")
+    (license license:expat)))
+
+(define-public rust-lv2-midi-1
+  (package
+    (name "rust-lv2-midi")
+    (version "1.2.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "lv2-midi" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0x0glbrfri1glgcrmvc6i1jfv6azhpqvp4ibk5cihsq3s2yfc8xd"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-lv2-atom" ,rust-lv2-atom-2)
+                       ("rust-lv2-sys" ,rust-lv2-sys-2)
+                       ("rust-urid" ,rust-urid-0.1)
+                       ("rust-wmidi" ,rust-wmidi-3))))
+    (home-page "https://github.com/RustAudio/rust-lv2")
+    (synopsis "rust-lv2's MIDI processing library")
+    (description "rust-lv2's MIDI processing library")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-lv2-units-0.1
+  (package
+    (name "rust-lv2-units")
+    (version "0.1.3")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "lv2-units" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0fdamp3hxdr36hqi1j6y01rz1x17if1ibzr7rr4nrabidw74gf82"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-lv2-sys" ,rust-lv2-sys-2)
+                       ("rust-urid" ,rust-urid-0.1))))
+    (home-page "https://github.com/RustAudio/rust-lv2")
+    (synopsis "rust-lv2's wrapper of LV2's unit types")
+    (description "rust-lv2's wrapper of LV2's unit types")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-urid-derive-0.1
+  (package
+    (name "rust-urid-derive")
+    (version "0.1.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "urid-derive" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0i1nf0sgq4ai051h17s9msaavl3jfzdmdlsy8455pr88y0pfx7l1"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-proc-macro2" ,rust-proc-macro2-1)
+                       ("rust-quote" ,rust-quote-1)
+                       ("rust-syn" ,rust-syn-1))))
+    (home-page "https://github.com/RustAudio/rust-lv2")
+    (synopsis "Procedural macros for urid")
+    (description "Procedural macros for urid")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-urid-0.1
+  (package
+    (name "rust-urid")
+    (version "0.1.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "urid" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "195672gs136vczn1r4hkjg5vfa7vdzr26bzv6lwhk0z7cvbvaa38"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-urid-derive" ,rust-urid-derive-0.1))))
+    (home-page "https://github.com/RustAudio/rust-lv2")
+    (synopsis "Library for idiomatic URID support")
+    (description "Library for idiomatic URID support")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-lv2-sys-2
+  (package
+    (name "rust-lv2-sys")
+    (version "2.0.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "lv2-sys" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0c4f59mrjyy0z0wf033wp648df0sc6zirrcd6kndqj9nvvkzkl4x"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t))
+    (home-page "https://github.com/RustAudio/rust-lv2")
+    (synopsis "rust-lv2's C header bindings")
+    (description "rust-lv2's C header bindings")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-lv2-core-derive-2
+  (package
+    (name "rust-lv2-core-derive")
+    (version "2.1.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "lv2-core-derive" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "12w3l41jzargrcywz13hbmaazfw4ix2sljl3601h6jfbdrw8zybv"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-proc-macro2" ,rust-proc-macro2-1)
+                       ("rust-quote" ,rust-quote-1)
+                       ("rust-syn" ,rust-syn-1))))
+    (home-page "https://github.com/RustAudio/rust-lv2")
+    (synopsis "Procedural macros for lv2-core")
+    (description "Procedural macros for lv2-core")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-lv2-core-3
+  (package
+    (name "rust-lv2-core")
+    (version "3.0.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "lv2-core" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1pj9l15zwqwj2h83f3xfpwxsj70vvhkw52gyzkljafvrbx1h00fm"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-lv2-core-derive" ,rust-lv2-core-derive-2)
+                       ("rust-lv2-sys" ,rust-lv2-sys-2)
+                       ("rust-urid" ,rust-urid-0.1))))
+    (home-page "https://github.com/RustAudio/rust-lv2")
+    (synopsis "rust-lv2's core library")
+    (description "rust-lv2's core library")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-lv2-atom-2
+  (package
+    (name "rust-lv2-atom")
+    (version "2.0.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "lv2-atom" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0wd9rgsn8sag8wyhjccmnn82gx4w1yyiav52nyvk579l21xlw6wm"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-lv2-core" ,rust-lv2-core-3)
+                       ("rust-lv2-sys" ,rust-lv2-sys-2)
+                       ("rust-lv2-units" ,rust-lv2-units-0.1)
+                       ("rust-urid" ,rust-urid-0.1))))
+    (home-page "https://github.com/RustAudio/rust-lv2")
+    (synopsis "rust-lv2's Atom handling library")
+    (description "rust-lv2's Atom handling library")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-lv2-0.6
+  (package
+    (name "rust-lv2")
+    (version "0.6.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "lv2" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1xh4hjfh2w5rhzbk0g9845k25f6fxrv7xqpkr09p0x57b200qc41"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-lv2-atom" ,rust-lv2-atom-2)
+                       ("rust-lv2-core" ,rust-lv2-core-3)
+                       ("rust-lv2-midi" ,rust-lv2-midi-1)
+                       ("rust-lv2-state" ,rust-lv2-state-2)
+                       ("rust-lv2-sys" ,rust-lv2-sys-2)
+                       ("rust-lv2-time" ,rust-lv2-time-0.1)
+                       ("rust-lv2-units" ,rust-lv2-units-0.1)
+                       ("rust-lv2-urid" ,rust-lv2-urid-2)
+                       ("rust-lv2-worker" ,rust-lv2-worker-0.1)
+                       ("rust-urid" ,rust-urid-0.1))))
+    (home-page "https://github.com/RustAudio/rust-lv2")
+    (synopsis "A safe, fast, and ergonomic framework to create LV2 plugins")
+    (description
+     "This package provides a safe, fast, and ergonomic framework to create LV2
+plugins")
+    (license (list license:expat license:asl2.0))))
 
 (define-public rust-bankstown
   (package

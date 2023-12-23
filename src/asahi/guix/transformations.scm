e75b01f2a091e75ac05a2036de7209bac1452111
Roman Scherer <roman@burningswell.com>
2023-12-23 18:15:57 +0100

    Replace mesa utils as well

--- src/asahi/guix/transformations.scm
+++ src/asahi/guix/transformations.scm
@@ -1,6 +1,7 @@
 (define-module (asahi guix transformations)
   #:use-module ((gnu packages gl) #:prefix gl:)
   #:use-module ((gnu packages rust) #:prefix rust:)
+  #:use-module ((gnu packages xorg))
   #:use-module (asahi guix packages gl)
   #:use-module (asahi guix packages rust)
   #:use-module (guix packages)
@@ -10,7 +11,10 @@
 (define replace-asahi
   (package-input-rewriting
    `((,gl:mesa . ,asahi-mesa)
+     (,gl:mesa-utils . ,asahi-mesa-utils)
      (,rust:rust . ,rust))))
 
 (define replace-mesa
-  (package-input-rewriting `((,gl:mesa . ,asahi-mesa))))
+  (package-input-rewriting
+   `((,gl:mesa . ,asahi-mesa)
+     (,gl:mesa-utils . ,asahi-mesa-utils))))

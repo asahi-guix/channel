a95e2fb21c7909815ad258173ede66169db113dc
Roman Scherer <roman@burningswell.com>
2024-02-24 20:19:08 +0100

    Remove asahi-icecat

--- src/asahi/guix/packages/gnuzilla.scm
+++
@@ -1,8 +0,0 @@
-(define-module (asahi guix packages gnuzilla)
-  #:use-module ((gnu packages gnuzilla) #:prefix gnu:)
-  #:use-module (asahi guix packages rust)
-  #:use-module (guix packages))
-
-(define-public asahi-icecat
-  (package/inherit gnu:icecat
-    (name "asahi-icecat")))

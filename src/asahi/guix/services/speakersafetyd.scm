5790b1a6e8c51f0b4889587489ff7469f0a0787f
Roman Scherer <roman@burningswell.com>
2024-02-25 20:01:09 +0100

    Add speakersafetyd-configuration

--- src/asahi/guix/services/speakersafetyd.scm
+++ src/asahi/guix/services/speakersafetyd.scm
@@ -1,24 +1,33 @@
 (define-module (asahi guix services speakersafetyd)
   #:use-module (asahi guix packages crates-io)
-  #:use-module (gnu services)
   #:use-module (gnu services shepherd)
+  #:use-module (gnu services)
   #:use-module (guix gexp)
+  #:use-module (guix records)
   #:export (speakersafetyd-service-type))
 
+(define-record-type* <speakersafetyd-configuration>
+  speakersafetyd-configuration
+  make-speakersafetyd-configuration
+  speakersafetyd-configuration?
+  (package speakersafetyd-configuration-package
+           (default rust-speakersafetyd-0.1)))
+
 (define (speakersafetyd-shepherd-service config)
-  (list (shepherd-service
-         (documentation "Run the speaker saftey daemon.")
-         (provision '(speakersafetyd))
-         (start #~(make-forkexec-constructor
-                   (list #$(file-append rust-speakersafetyd-0.1 "/bin/speakersafetyd"))))
-         (stop #~(make-kill-destructor)))))
+  (let ((speakersafetyd (speakersafetyd-configuration-package config)))
+    (list (shepherd-service
+           (documentation "Asahi Speaker Saftey daemon")
+           (provision '(speakersafetyd))
+           (start #~(make-forkexec-constructor
+                     (list (string-append #$speakersafetyd "/bin/speakersafetyd"))))
+           (stop #~(make-kill-destructor))))))
 
 (define speakersafetyd-service-type
   (service-type
    (name 'speakersafetyd)
-   (description "Run the speaker saftey daemon.")
+   (description "Asahi Speaker Saftey daemon")
    (extensions
     (list (service-extension
            shepherd-root-service-type
            speakersafetyd-shepherd-service)))
-   (default-value #f)))
+   (default-value (speakersafetyd-configuration))))

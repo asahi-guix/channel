60e1bfe8a8939a5932a4d191f3344db8902e8f45
Roman Scherer <roman@burningswell.com>
2024-03-03 13:19:36 +0100

    Add boot logo

--- src/asahi/guix/packages/bootloader.scm
+++ src/asahi/guix/packages/bootloader.scm
@@ -1,12 +1,25 @@
 (define-module (asahi guix packages bootloader)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages bootloaders)
+  #:use-module (gnu packages imagemagick)
   #:use-module (gnu packages tls)
+  #:use-module (guix build-system copy)
   #:use-module (guix build-system gnu)
   #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module (guix packages)
   #:use-module (guix utils))
+
+(define-public asahi-bootlogo
+  (package
+    (name "asahi-bootlogo")
+    (version "0.0.1")
+    (source (local-file "../files/asahi-bootlogo.svg"))
+    (build-system copy-build-system)
+    (home-page "https://www.gnu.org/graphics/heckert_gnu.html")
+    (synopsis "Asahi Guix boot logo")
+    (description "A modified version of the famous GNU logo for the U-Boot.")
+    (license license:expat)))
 
 (define-public asahi-m1n1
   (package
@@ -27,6 +40,27 @@
      (list
       #:phases
       #~(modify-phases %standard-phases
+          (add-after 'unpack 'bootlogo
+            (lambda* (#:key inputs outputs #:allow-other-keys)
+              (let* ((out (assoc-ref outputs "out"))
+                     (bootlogo (assoc-ref inputs "asahi-bootlogo"))
+                     (bootlogo-input (string-append bootlogo "/asahi-bootlogo.svg")))
+                (when (file-exists? bootlogo-input)
+                  (chdir "data")
+                  (delete-file "bootlogo_128.png")
+                  (invoke "convert"
+                          "-background" "none"
+                          "-resize" "128x128!"
+                          bootlogo-input
+                          "bootlogo_128.png")
+                  (delete-file "bootlogo_256.png")
+                  (invoke "convert"
+                          "-background" "none"
+                          "-resize" "256x256!"
+                          bootlogo-input
+                          "bootlogo_256.png")
+                  (invoke "sh" "makelogo.sh")
+                  (chdir "..")))))
           (replace 'configure
             (lambda _
               (setenv "RELEASE" "1")))
@@ -37,6 +71,7 @@
                 (copy-file "build/m1n1.bin" (string-append dir "m1n1.bin")))))
           ;; There are no tests
           (delete 'check))))
+    (native-inputs (list asahi-bootlogo imagemagick))
     (home-page "https://github.com/AsahiLinux/m1n1")
     (synopsis "Boot loader and experimentation playground for Apple Silicon")
     (description "m1n1 is the bootloader developed by the Asahi Linux project to bridge

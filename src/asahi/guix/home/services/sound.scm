a4ad3040ba0f58cfae12ed8188781bbdedfc7e61
Roman Scherer <roman@burningswell.com>
2024-03-17 19:17:11 +0100

    Extract some helper fns

--- src/asahi/guix/home/services/sound.scm
+++ src/asahi/guix/home/services/sound.scm
@@ -43,6 +43,19 @@
    "When true, enable PipeWire's PulseAudio emulation support, allowing
 PulseAudio clients to use PipeWire transparently."))
 
+(define (alsa-config-ucm2 config)
+  #~(string-append #$(home-pipewire-configuration-alsa-ucm-conf config)
+                   "/share/alsa/ucm2"))
+
+(define (lv2-path config)
+  #~(string-append
+     #$(home-pipewire-configuration-bankstown config) "/lib/lv2" ":"
+     #$(home-pipewire-configuration-lsp-plugins config) "/lib/lv2"))
+
+(define (pipewire-module-dir config)
+  #~(string-append #$(home-pipewire-configuration-pipewire config)
+                   "/lib/pipewire-0.3"))
+
 (define (home-pipewire-shepherd-service config)
   (shepherd-service
    (documentation "PipeWire media processing.")
@@ -54,22 +67,13 @@ PulseAudio clients to use PipeWire transparently."))
                       "/bin/pipewire"))
              #:environment-variables
              (list "DISABLE_RTKIT=1"
-                   (string-append
-                    "ALSA_CONFIG_UCM2="
-                    #$(home-pipewire-configuration-alsa-ucm-conf config)
-                    "/share/alsa/ucm2")
-                   (string-append
-                    "LV2_PATH="
-                    #$(home-pipewire-configuration-bankstown config)
-                    "/lib/lv2"
-                    ":"
-                    #$(home-pipewire-configuration-lsp-plugins config)
-                    "/lib/lv2")
+                   (string-append "ALSA_CONFIG_UCM2="
+                                  #$(alsa-config-ucm2 config))
+                   (string-append "LV2_PATH="
+                                  #$(lv2-path config))
                    "PIPEWIRE_DEBUG=I"
-                   (string-append
-                    "PIPEWIRE_MODULE_DIR="
-                    #$(home-pipewire-configuration-pipewire config)
-                    "/lib/pipewire-0.3")
+                   (string-append "PIPEWIRE_MODULE_DIR="
+                                  #$(pipewire-module-dir config))
                    (string-append
                     "XDG_RUNTIME_DIR="
                     (or (getenv "XDG_RUNTIME_DIR")
@@ -87,22 +91,13 @@ PulseAudio clients to use PipeWire transparently."))
                       "/bin/pipewire-pulse"))
              #:environment-variables
              (list "DISABLE_RTKIT=1"
-                   (string-append
-                    "ALSA_CONFIG_UCM2="
-                    #$(home-pipewire-configuration-alsa-ucm-conf config)
-                    "/share/alsa/ucm2")
-                   (string-append
-                    "LV2_PATH="
-                    #$(home-pipewire-configuration-bankstown config)
-                    "/lib/lv2"
-                    ":"
-                    #$(home-pipewire-configuration-lsp-plugins config)
-                    "/lib/lv2")
+                   (string-append "ALSA_CONFIG_UCM2="
+                                  #$(alsa-config-ucm2 config))
+                   (string-append "LV2_PATH="
+                                  #$(lv2-path config))
                    "PIPEWIRE_DEBUG=I"
-                   (string-append
-                    "PIPEWIRE_MODULE_DIR="
-                    #$(home-pipewire-configuration-pipewire config)
-                    "/lib/pipewire-0.3")
+                   (string-append "PIPEWIRE_MODULE_DIR="
+                                  #$(pipewire-module-dir config))
                    (string-append
                     "XDG_RUNTIME_DIR="
                     (or (getenv "XDG_RUNTIME_DIR")
@@ -120,17 +115,10 @@ PulseAudio clients to use PipeWire transparently."))
                       "/bin/wireplumber"))
              #:environment-variables
              (list "DISABLE_RTKIT=1"
-                   (string-append
-                    "ALSA_CONFIG_UCM2="
-                    #$(home-pipewire-configuration-alsa-ucm-conf config)
-                    "/share/alsa/ucm2")
-                   (string-append
-                    "LV2_PATH="
-                    #$(home-pipewire-configuration-bankstown config)
-                    "/lib/lv2"
-                    ":"
-                    #$(home-pipewire-configuration-lsp-plugins config)
-                    "/lib/lv2")
+                   (string-append "ALSA_CONFIG_UCM2="
+                                  #$(alsa-config-ucm2 config))
+                   (string-append "LV2_PATH="
+                                  #$(lv2-path config))
                    "WIREPLUMBER_DEBUG=I"
                    (string-append
                     "XDG_RUNTIME_DIR="
@@ -147,7 +135,7 @@ PulseAudio clients to use PipeWire transparently."))
 
 (define (home-pipewire-asoundrc config)
   (match-record config <home-pipewire-configuration>
-                (pipewire)
+    (pipewire)
     (mixed-text-file
      "asoundrc"
      "<" pipewire "/share/alsa/alsa.conf.d/50-pipewire.conf>\n"
@@ -217,11 +205,16 @@ PulseAudio clients to use PipeWire transparently."))
         (home-pipewire-configuration-pipewire config)
         (home-pipewire-configuration-wireplumber config)))
 
+(define (home-pipewire-environment-variables config)
+  `(("ALSA_CONFIG_UCM2" . ,(alsa-config-ucm2 config))))
+
 (define home-pipewire-service-type
   (service-type
    (name 'pipewire)
    (extensions
-    (list (service-extension home-profile-service-type
+    (list (service-extension home-environment-variables-service-type
+                             home-pipewire-environment-variables)
+          (service-extension home-profile-service-type
                              home-pipewire-profile-entries)
           (service-extension home-shepherd-service-type
                              home-pipewire-shepherd-services)

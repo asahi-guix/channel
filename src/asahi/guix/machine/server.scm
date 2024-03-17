16fb385047f67795d2081db5bb03196f76dd3029
Roman Scherer <roman@burningswell.com>
2024-03-17 16:26:40 +0100

    Add host key, use ed25519 identity

--- src/asahi/guix/machine/server.scm
+++ src/asahi/guix/machine/server.scm
@@ -5,15 +5,20 @@
   #:use-module (gnu)
   #:export (asahi-guix-server-machine))
 
+(define host-key
+  (string-append "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAhdngke8Fhf7ruQFij8riXXiFIfkPi/XiX262TzTLYM root@(none)"))
+
 (define asahi-guix-server-machine
   (machine
    (operating-system asahi-guix-server-system)
    (environment managed-host-environment-type)
    (configuration (machine-ssh-configuration
+                   (build-locally? #f)
+                   (host-key host-key)
                    (host-name "www.asahi-guix.org")
+                   (identity "/home/roman/.ssh/id_ed25519")
+                   (port 22)
                    (system "aarch64-linux")
-                   (user "root")
-                   (identity "/home/roman/.ssh/id_rsa")
-                   (port 22)))))
+                   (user "root")))))
 
 (list asahi-guix-server-machine)

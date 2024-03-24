01989826a147ff11eeacfece07f351f173f19620
Roman Scherer <roman@burningswell.com>
2024-03-24 20:28:09 +0100

    Build manifest

--- src/asahi/guix/system/server.scm
+++ src/asahi/guix/system/server.scm
@@ -224,6 +224,29 @@ COMMIT
                                 "3eeb493b037bea44f225c4314c5556aa25aff36c"
                                 (openpgp-fingerprint
                                  "D226 A339 D8DF 4481 5DDE  0CA0 3DDA 5252 7D2A C199"))))))
+                      (systems '("aarch64-linux")))
+                     (specification
+                      (name "manifset")
+                      (build '(manifest ".guix/manifest.scm"))
+                      (channels
+                       (list (channel
+                              (name 'guix)
+                              (url "https://github.com/asahi-guix/guix")
+                              (branch "main")
+                              (introduction
+                               (make-channel-introduction
+                                "3a2835615d22d9a0f20667837083625b96c0f1b3"
+                                (openpgp-fingerprint
+                                 "D226 A339 D8DF 4481 5DDE  0CA0 3DDA 5252 7D2A C199"))))
+                             (channel
+                              (name 'asahi)
+                              (branch "main")
+                              (url "https://github.com/asahi-guix/channel")
+                              (introduction
+                               (make-channel-introduction
+                                "3eeb493b037bea44f225c4314c5556aa25aff36c"
+                                (openpgp-fingerprint
+                                 "D226 A339 D8DF 4481 5DDE  0CA0 3DDA 5252 7D2A C199"))))))
                       (systems '("aarch64-linux")))))
             (use-substitutes? #t)
             (remote-server

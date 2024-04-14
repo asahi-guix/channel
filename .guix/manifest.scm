51422c9b32b3bba131a5da9c2a2d05615ec02827
Roman Scherer <roman@burningswell.com>
2024-04-14 19:26:54 +0200

    Build installer and edge systems

--- .guix/manifest.scm
+++ .guix/manifest.scm
@@ -1,41 +1,39 @@
 (use-modules (asahi guix initrd)
              (asahi guix packages linux)
              (asahi guix system base)
+             (asahi guix system install)
              (gnu packages base)
              (gnu system)
              (guix gexp)
              (guix packages)
              (guix profiles))
 
-(define* (package->manifest-entry* package system
-                                   #:key target)
-  "Return a manifest entry for PACKAGE on SYSTEM, optionally cross-compiled to
-TARGET."
-  (manifest-entry
-    (inherit (package->manifest-entry package))
-    (name (string-append (package-name package) "." system
-                         (if target
-                             (string-append "." target)
-                             "")))
-    (item (with-parameters ((%current-system system)
-                            (%current-target-system target))
-            package))))
+(define %asahi-guix-version "0.1")
 
-(define native-builds
-  (manifest
-   (append (map (lambda (system)
-                  (package->manifest-entry* hello system))
-                '("aarch64-linux")))))
+(define %asahi-guix-system
+  (manifest-entry
+    (name "asahi-guix-system")
+    (version %asahi-guix-version)
+    (item (asahi-operating-system
+           #:esp-uuid "41F0-16FF"))))
+
+(define %asahi-guix-system-edge
+  (manifest-entry
+    (name "asahi-guix-system-edge")
+    (version %asahi-guix-version)
+    (item (asahi-operating-system
+           #:esp-uuid "41F0-16FF"
+           #:initrd-modules asahi-initrd-modules-edge
+           #:kernel asahi-linux-edge))))
+
+(define %asahi-guix-installer
+  (manifest-entry
+    (name "asahi-guix-installer")
+    (version %asahi-guix-version)
+    (item asahi-installation-operating-system)))
 
 (concatenate-manifests
  (list (manifest
-        (list (manifest-entry
-                (name "asahi-guix-system")
-                (version "1")
-                (item (asahi-operating-system
-                       #:esp-uuid "41F0-16FF"
-                       #:initrd-modules asahi-initrd-modules-edge
-                       #:kernel asahi-linux-edge)))))))
-
-
-;; (concatenate-manifests (list native-builds))
+        (list %asahi-guix-system
+              %asahi-guix-system-edge
+              %asahi-guix-installer))))

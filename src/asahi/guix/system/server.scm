ff84c2111aa09742f1aee1bdf197ea3c3f1b763d
Roman Scherer <roman@burningswell.com>
2024-05-05 16:08:54 +0200

    Fix manifests build entry

--- src/asahi/guix/system/server.scm
+++ src/asahi/guix/system/server.scm
@@ -231,7 +231,7 @@ COMMIT
            (systems '("aarch64-linux")))
           (specification
            (name "manifest")
-           (build '(manifest ".guix/manifest.scm"))
+           (build '(manifests ".guix/manifest.scm"))
            (channels
             (list (channel
                    (name 'guix)

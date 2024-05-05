25e53d5c298fce7116d6febe4d6d83251b0f52af
Roman Scherer <roman@burningswell.com>
2024-05-05 15:58:38 +0200

    Build less packages

--- src/asahi/guix/system/server.scm
+++ src/asahi/guix/system/server.scm
@@ -167,10 +167,10 @@ COMMIT
   #~(list (specification
            (name "guix")
            (build '(packages
-                    "gnome-desktop"
-                    "icecat"
+                    ;; "gnome-desktop"
+                    ;; "icecat"
                     "jemalloc"
-                    "plasma-desktop"
+                    ;; "plasma-desktop"
                     "rust"))
            (channels
             (list (channel

bdd42a2bafb1212e47cf247f9adb459fc1b39e9c
Roman Scherer <roman@burningswell.com>
2024-05-05 18:59:14 +0200

    Add asahi-libclc

---
+++ src/asahi/guix/packages/llvm.scm
@@ -0,0 +1,13 @@
+(define-module (asahi guix packages llvm)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages llvm)
+  #:use-module (guix packages))
+
+(define-public asahi-libclc
+  (package/inherit libclc
+    (name "asahi-libclc")
+    (version (package-version llvm-18))
+    (native-inputs
+     (modify-inputs (package-native-inputs libclc)
+       (replace "clang" clang-15)
+       (replace "llvm" llvm-15)))))

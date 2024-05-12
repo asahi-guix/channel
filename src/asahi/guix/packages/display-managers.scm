5d70e6e638291efad08c4fe3d2f0696e733577bc
Roman Scherer <roman@burningswell.com>
2024-05-12 16:39:12 +0200

    Add asahi-slim package

---
+++ src/asahi/guix/packages/display-managers.scm
@@ -0,0 +1,11 @@
+(define-module (asahi guix packages display-managers)
+  #:use-module (asahi guix packages gl)
+  #:use-module (gnu packages display-managers)
+  #:use-module (guix packages))
+
+(define-public asahi-slim
+  (package/inherit slim
+    (name "asahi-slim")
+    (inputs
+     (modify-inputs (package-inputs slim)
+       (replace "mesa" asahi-mesa)))))

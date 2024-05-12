648d5c22f43d3329f2dbe0f000235d0a94e54a00
Roman Scherer <roman@burningswell.com>
2024-05-12 10:06:31 +0200

    Don't build of desktops and browsers for now

--- src/asahi/guix/system/server.scm
+++ src/asahi/guix/system/server.scm
@@ -167,11 +167,11 @@ COMMIT
   #~(list (specification
            (name "guix")
            (build '(packages
-                    "gnome-desktop"
-                    "icecat"
+                    ;; "gnome-desktop"
+                    ;; "icecat"
                     "jemalloc"
-                    "librewolf"
-                    "plasma-desktop"
+                    ;; "librewolf"
+                    ;; "plasma-desktop"
                     "rust"))
            (channels
             (list (channel

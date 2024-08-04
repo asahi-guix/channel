f55badae23e6b57bcb3d567b90ae90d9d9cf3b51
Roman Scherer <roman@burningswell.com>
2024-08-04 12:44:51 +0200

    Update channel introduction

--- src/asahi/guix/channels.scm
+++ src/asahi/guix/channels.scm
@@ -34,7 +34,7 @@
    (branch "main")
    (introduction
     (make-channel-introduction
-     "47ad87a9190dd8710121fd24b0e9169dd828bfa8"
+     "cfba8d90682d0d71c60b32c4217edc089490b923"
      (openpgp-fingerprint
       "D226 A339 D8DF 4481 5DDE  0CA0 3DDA 5252 7D2A C199")))))
 

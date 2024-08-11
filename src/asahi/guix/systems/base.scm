243fca420f46482c651764f7727536ffc5cf704b
Roman Scherer <roman@burningswell.com>
2024-08-11 15:28:24 +0200

    Refactor base module

--- src/asahi/guix/systems/base.scm
+++ src/asahi/guix/systems/base.scm
@@ -30,69 +30,79 @@
   #:use-module (guix packages)
   #:use-module (ice-9 optargs))
 
-(define %kernel-arguments
+(define %asahi-base-bootloader
+  (bootloader-configuration
+   (bootloader m1n1-u-boot-grub-bootloader)
+   (targets (list "/boot/efi"))
+   (keyboard-layout %asahi-keyboard-layout)))
+
+(define %asahi-guest-user
+  (user-account
+   (name "guest")
+   (comment "Guest")
+   (group "users")
+   (supplementary-groups '("audio" "netdev" "video" "wheel"))))
+
+(define %asahi-default-kernel-arguments
   (append '("net.ifnames=0") %default-kernel-arguments))
 
-(define %keyboard-options
-  '("caps:ctrl_modifier"
-    "terminate:ctrl_alt_bksp"))
+(define %asahi-keyboard-options
+  '("caps:ctrl_modifier" "terminate:ctrl_alt_bksp"))
 
-(define %keyboard-layout
-  (keyboard-layout "us" #:options %keyboard-options))
+(define %asahi-keyboard-layout
+  (keyboard-layout "us" #:options %asahi-keyboard-options))
 
-(define %file-systems
-  (cons* (file-system
-           (device (file-system-label "EFI - UEFI"))
-           (mount-point "/boot/efi")
-           (needed-for-boot? #t)
-           (type "vfat"))
-         (file-system
-           (device (file-system-label "asahi-guix-root"))
-           (mount-point "/")
-           (needed-for-boot? #t)
-           (type "ext4"))
+(define %asahi-base-file-system-boot
+  (file-system
+    (device (file-system-label "EFI - UEFI"))
+    (mount-point "/boot/efi")
+    (needed-for-boot? #t)
+    (type "vfat")))
+
+(define %asahi-base-file-system-root
+  (file-system
+    (device (file-system-label "asahi-guix-root"))
+    (mount-point "/")
+    (needed-for-boot? #t)
+    (type "ext4")))
+
+(define %asahi-base-file-systems
+  (cons* %asahi-base-file-system-boot
+         %asahi-base-file-system-root
          %base-file-systems))
 
-(define %packages
+(define %asahi-base-packages
   (cons* e2fsprogs network-manager %base-packages))
 
-(define %openssh-configuration
+(define %asahi-base-openssh-configuration
   (openssh-configuration (openssh openssh-sans-x)))
 
-(define %services
+(define %asahi-base-services
   (modify-services (cons* (service asahi-firmware-service-type)
                           (service network-manager-service-type)
-                          (service openssh-service-type %openssh-configuration)
+                          (service openssh-service-type %asahi-base-openssh-configuration)
                           (service wpa-supplicant-service-type)
                           %base-services)
     (console-font-service-type config => (console-font-terminus config))
     (guix-service-type config => (append-substitutes config))))
 
-(define %users
-  (cons (user-account
-         (name "guest")
-         (comment "Guest")
-         (group "users")
-         (supplementary-groups '("audio" "netdev" "video" "wheel")))
-        %base-user-accounts))
+(define %asahi-base-user-accounts
+  (cons %asahi-guest-user %base-user-accounts))
 
 (define-public asahi-base-os
   (operating-system
     (host-name "asahi-guix")
     (locale "en_US.utf8")
     (timezone "Europe/Berlin")
-    (keyboard-layout %keyboard-layout)
-    (bootloader (bootloader-configuration
-                 (bootloader m1n1-u-boot-grub-bootloader)
-                 (targets (list "/boot/efi"))
-                 (keyboard-layout %keyboard-layout)))
-    (kernel asahi-linux-edge)
-    (kernel-arguments %kernel-arguments)
-    (initrd-modules asahi-initrd-modules-edge)
-    (file-systems %file-systems)
-    (packages %packages)
-    (services %services)
-    (users %users)))
+    (keyboard-layout %asahi-keyboard-layout)
+    (bootloader %asahi-base-bootloader)
+    (kernel asahi-linux)
+    (kernel-arguments %asahi-default-kernel-arguments)
+    (initrd-modules asahi-initrd-modules)
+    (file-systems %asahi-base-file-systems)
+    (packages %asahi-base-packages)
+    (services %asahi-base-services)
+    (users %asahi-base-user-accounts)))
 
 (define-public asahi-edge-os
   (operating-system

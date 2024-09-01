f3930562c44e2dfc85b28aefec370acf0a2d0094
Roman Scherer <roman@burningswell.com>
2024-09-01 14:34:32 +0200

    Don't build desktops for now

--- .guix/manifest.scm
+++ .guix/manifest.scm
@@ -1,5 +1,5 @@
 (use-modules (asahi guix systems base)
-             (asahi guix systems desktop)
+             ;; (asahi guix systems desktop)
              ;; (asahi guix systems install)
              (gnu packages base)
              (gnu system)
@@ -22,11 +22,11 @@
     (version %asahi-guix-version)
     (item asahi-edge-os)))
 
-(define %asahi-guix-gnome
-  (manifest-entry
-    (name "asahi-guix-gnome")
-    (version %asahi-guix-version)
-    (item asahi-gnome-os)))
+;; (define %asahi-guix-gnome
+;;   (manifest-entry
+;;     (name "asahi-guix-gnome")
+;;     (version %asahi-guix-version)
+;;     (item asahi-gnome-os)))
 
 ;; (define %asahi-guix-installer
 ;;   (manifest-entry
@@ -34,17 +34,17 @@
 ;;     (version %asahi-guix-version)
 ;;     (item asahi-installation-os)))
 
-(define %asahi-guix-plasma
-  (manifest-entry
-    (name "asahi-guix-plasma")
-    (version %asahi-guix-version)
-    (item asahi-plasma-os)))
+;; (define %asahi-guix-plasma
+;;   (manifest-entry
+;;     (name "asahi-guix-plasma")
+;;     (version %asahi-guix-version)
+;;     (item asahi-plasma-os)))
 
-(define %asahi-guix-sway
-  (manifest-entry
-    (name "asahi-guix-sway")
-    (version %asahi-guix-version)
-    (item asahi-sway-os)))
+;; (define %asahi-guix-sway
+;;   (manifest-entry
+;;     (name "asahi-guix-sway")
+;;     (version %asahi-guix-version)
+;;     (item asahi-sway-os)))
 
 (define %asahi-packages
   (packages->manifest (list hello which)))
@@ -52,8 +52,9 @@
 (define %asahi-systems
   (manifest (list %asahi-guix-base
                   %asahi-guix-edge
-                  %asahi-guix-gnome
+                  ;; %asahi-guix-gnome
                   ;; %asahi-guix-installer
-                  %asahi-guix-plasma)))
+                  ;; %asahi-guix-plasma
+                  )))
 
 (concatenate-manifests (list %asahi-packages %asahi-systems))

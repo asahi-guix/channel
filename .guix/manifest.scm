dc19cb61da962e39d2fc7aca8b3539661a639e4c
Roman Scherer <roman@burningswell.com>
2024-09-01 13:30:28 +0200

    Use (asahi guix systems base) in manifest

--- .guix/manifest.scm
+++ .guix/manifest.scm
@@ -1,7 +1,7 @@
-(use-modules (asahi guix initrd)
-             (asahi guix packages linux)
+(use-modules ;; (asahi guix initrd)
+             ;; (asahi guix packages linux)
              (asahi guix systems base)
-             (asahi guix systems install)
+             ;; (asahi guix systems install)
              (gnu packages base)
              (gnu system)
              (guix gexp)
@@ -10,30 +10,30 @@
              (guix profiles)
              (guix transformations))
 
-(define %asahi-guix-version "0.1")
+;; (define %asahi-guix-version "0.1")
 
-(define %asahi-guix-base
-  (manifest-entry
-    (name "asahi-guix-base")
-    (version %asahi-guix-version)
-    (item asahi-base-os)))
+;; (define %asahi-guix-base
+;;   (manifest-entry
+;;     (name "asahi-guix-base")
+;;     (version %asahi-guix-version)
+;;     (item asahi-base-os)))
 
-(define %asahi-guix-edge
-  (manifest-entry
-    (name "asahi-guix-edge")
-    (version %asahi-guix-version)
-    (item asahi-edge-os)))
+;; (define %asahi-guix-edge
+;;   (manifest-entry
+;;     (name "asahi-guix-edge")
+;;     (version %asahi-guix-version)
+;;     (item asahi-edge-os)))
 
-(define %asahi-guix-installer
-  (manifest-entry
-    (name "asahi-guix-installer")
-    (version %asahi-guix-version)
-    (item asahi-installation-os)))
+;; (define %asahi-guix-installer
+;;   (manifest-entry
+;;     (name "asahi-guix-installer")
+;;     (version %asahi-guix-version)
+;;     (item asahi-installation-os)))
 
-(concatenate-manifests
- (list (manifest
-        (list %asahi-guix-base
-              %asahi-guix-edge
-              %asahi-guix-installer))))
+;; (concatenate-manifests
+;;  (list (manifest
+;;         (list %asahi-guix-base
+;;               %asahi-guix-edge
+;;               %asahi-guix-installer))))
 
 (packages->manifest (specifications->packages '("hello")))

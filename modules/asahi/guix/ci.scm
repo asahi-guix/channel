c89442e650c23ccef56889c717b5e4a709b724bb
Roman Scherer <roman@burningswell.com>
2024-09-15 17:39:20 +0200

    Add asahi-packages

--- modules/asahi/guix/ci.scm
+++ modules/asahi/guix/ci.scm
@@ -1,9 +1,25 @@
 (define-module (asahi guix ci)
-  #:use-module (gnu ci)
-  #:use-module (gnu system image)
   #:use-module (asahi guix images installer)
+  #:use-module (gnu ci)
+  #:use-module (gnu packages)
+  #:use-module (gnu system image)
+  #:use-module (guix diagnostics)
+  #:use-module (guix discovery)
+  #:use-module (guix packages)
   #:use-module (srfi srfi-1)
   #:export (cuirass-jobs))
+
+(define (asahi-package? package)
+  (let ((filename (location-file (package-location package))))
+    (string-prefix? "asahi/guix/packages" filename)))
+
+(define (asahi-packages)
+  (fold-packages (lambda (package result)
+                   (if (asahi-package? package)
+                       (cons package result)
+                       result))
+                 '()
+                 #:select? (const #t)))
 
 (define (cuirass-jobs store arguments)
   (display "Asahi Guix Curiass Jobs\n")
@@ -12,6 +28,7 @@
     (arguments->systems arguments))
 
   (format #t "Systems ~a\n" systems)
+  (format #t "Packages ~a\n" (asahi-packages))
 
   (append-map
    (lambda (system)

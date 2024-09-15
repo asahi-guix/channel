5b2e4a685ac1e7e9e511461993a23a5ec2aa3c22
Roman Scherer <roman@burningswell.com>
2024-09-15 17:40:53 +0200

    Add more packages to replace-asahi

--- modules/asahi/guix/transformations.scm
+++ modules/asahi/guix/transformations.scm
@@ -1,13 +1,14 @@
 (define-module (asahi guix transformations)
   #:use-module (asahi guix packages gl)
+  #:use-module (asahi guix packages linux)
   #:use-module (guix packages))
 
 (define-public replace-asahi
   (package-input-rewriting/spec
-   `(("mesa" . ,(const asahi-mesa))
-     ("mesa-utils" . ,(const asahi-mesa-utils)))))
-
-(define-public replace-mesa
-  (package-input-rewriting/spec
-   `(("mesa" . ,(const asahi-mesa))
-     ("mesa-utils" . ,(const asahi-mesa-utils)))))
+   `(("alsa-lib" . ,(const asahi-alsa-lib))
+     ("alsa-ucm-conf" . ,(const asahi-alsa-ucm-conf))
+     ("mesa" . ,(const asahi-mesa))
+     ("mesa-headers" . ,(const asahi-mesa-headers))
+     ("mesa-utils" . ,(const asahi-mesa-utils))
+     ("pipewire" . ,(const asahi-pipewire))
+     ("wireplumber" . ,(const asahi-wireplumber)))))

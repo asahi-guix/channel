843661fd272e1a3ca0152da59ff9825ffe82c3f3
Roman Scherer <roman@burningswell.com>
2024-10-06 11:23:20 +0200

    WIP mesa vulkan

--- modules/asahi/guix/packages/gl.scm
+++ modules/asahi/guix/packages/gl.scm
@@ -3,6 +3,8 @@
   #:use-module (asahi guix packages llvm)
   #:use-module (gnu packages freedesktop)
   #:use-module (gnu packages gl)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages llvm)
   #:use-module (gnu packages tls)
@@ -17,10 +19,10 @@
   #:use-module (guix utils))
 
 (define-public asahi-mesa
-  (let ((commit "6d00b02b727f89a324e7e3dcb34e6c74ef0848b3"))
+  (let ((commit "6e004ce87790404a61aedaaa4091c7329f579a8a"))
     (package/inherit mesa
       (name "asahi-mesa")
-      (version (git-version "20240727" "0" commit))
+      (version (git-version "20241006" "0" commit))
       (source
        (origin
          (method git-fetch)
@@ -29,15 +31,16 @@
                (commit commit)))
          (file-name (git-file-name name version))
          (sha256
-          (base32 "1ii6h5sjf2wyy2xqhymdswi37l0r7wqs904p9p50h9vhwy4ncy2x"))))
+          (base32 "06c1bngwlafvmnzvdga6j1b13r19jn687vnzfjgmhnicy0vm79pj"))))
       (arguments
        (substitute-keyword-arguments (package-arguments mesa)
          ((#:configure-flags flags)
           `(list "-Db_lto=false"
                  "-Db_ndebug=true"
                  "-Dbuild-tests=true"
-                 "-Ddri3=enabled"
+                 ;; "-Ddri3=enabled"
                  "-Degl=enabled"
+                 ;; "-Dgallium-drivers=asahi,kmsro,svga,swrast,virgl,zink"
                  "-Dgallium-drivers=asahi,kmsro,svga,swrast,virgl,zink"
                  "-Dgallium-extra-hud=true"
                  "-Dgallium-opencl=disabled"
@@ -64,7 +67,10 @@
       (inputs
        (modify-inputs (package-inputs mesa)
          (prepend `(,lm-sensors "lib"))
-         (replace "wayland-protocols" wayland-protocols-next))))))
+         (replace "wayland-protocols" wayland-protocols-next)))
+      (native-inputs
+       (modify-inputs (package-native-inputs mesa)
+         (prepend python python-pyyaml))))))
 
 (define-public asahi-mesa-headers
   (package/inherit mesa-headers

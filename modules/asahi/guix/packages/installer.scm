af9ee20e2f7392b38cf98ef32ff045ae6f3c6bc6
Roman Scherer <roman@burningswell.com>
2024-10-15 19:44:47 +0200

    Change installer data url

--- modules/asahi/guix/packages/installer.scm
+++ modules/asahi/guix/packages/installer.scm
@@ -83,9 +83,9 @@
             (lambda* _
               (substitute* "scripts/bootstrap-prod.sh"
                 (("https://github.com/AsahiLinux/asahi-installer/raw/prod/data/installer_data.json")
-                 "https://www.asahi-guix.org/os/installer_data.json")
+                 "https://www.asahi-guix.org/installer_data.json")
                 (("https://alx.sh/installer_data.json")
-                 "https://www.asahi-guix.org/os/installer_data.json")
+o                 "https://www.asahi-guix.org/installer_data.json")
                 (("https://stats.asahilinux.org/report")
                  "https://stats.asahi-guix.org/report")
                 (("REPO_BASE=https://cdn.asahilinux.org")

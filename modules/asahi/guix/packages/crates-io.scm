2a47d59f971131509c3202177638921138c3af16
Roman Scherer <roman@burningswell.com>
2025-01-09 12:27:37 +0100

    Import audio crates

--- modules/asahi/guix/packages/crates-io.scm
+++ modules/asahi/guix/packages/crates-io.scm
@@ -1,6 +1,7 @@
 (define-module (asahi guix packages crates-io)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (asahi guix packages linux)
+  #:use-module (gnu packages crates-audio)
   #:use-module (gnu packages crates-io)
   #:use-module (gnu packages crates-windows)
   #:use-module (gnu packages linux)
